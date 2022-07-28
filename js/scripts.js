0dd48add98393161c2119b85043e0156042bb969
shayalieb <105383935+shayalieb@users.noreply.github.com>
2022-07-28 10:55:57 -0400

    Updated the code
    To include the IIFE in the private variable
    and to allow the returns to show on the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,32 @@
 //This object variable will contain the Pokemon "name", "height", and type
 
 //start the IIFE fucntion
-(function () {
+let pokemonRepository = (function () {
   let pokemonList = [
     {name: 'Metapod', type: 'Bug', height: 0.7}, 
     {name: 'Weedle', type: ['bug', 'Poison'], height: 0.3},
     {name: 'Fearow', type: ['flying', 'normal'], height: 1.2},
     {name: 'Pikachu', type: 'Electric', height: 0.4}
   ];
+  
+  function add(pokemon) {//a function to add the new object
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {//to return the object anbd keys
+    return pokemonList;
+  }
+
+  return {//return the new object
+    add: add,
+    getAll: getAll
+  };
 })();
 
 //end the IIFE function
 
 //forEach is a pre defined function in JS. item => followed by a {} which indicates to start the function
-pokemonList.forEach(item => {  
+pokemonRepository.getAll().forEach(item => {  
   if (item.height > 0 && item.height < 0.4) {
       document.write('<h1>' + item.name + ' (height: ' + item.height + ') - This is a small Pokepmon!)</h1>') 
   } else if (item.height > 0.3 && item.height < 0.7) {
@@ -23,22 +36,6 @@ pokemonList.forEach(item => {
   }
 })
 
-//To pull the function using IIFE
-let pokemonRepository = (function () {
-  let  pokemonList = [];
-
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-
-  function getAll() {
-    return pokemonList;
-  }
-
-  return {
-    add: add,
-    getAll: getAll
-  };
-})()
+//
 
 
